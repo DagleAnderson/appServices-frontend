9ac11a9e42d049c3c6e30e52abe68bb761f91978
D'aglê Anderson <dagle_life@hotmail.com>
2018-11-19 21:59:25 -0300

    atualização da versão do eclipse,atualização de pom.xml e ajuste de Requestmapping de Cursos

--- src/main/java/com/appServices/AppServices/resources/CurriculoResource.java
+++ src/main/java/com/appServices/AppServices/resources/CurriculoResource.java
@@ -1,14 +1,26 @@
 package com.appServices.AppServices.resources;
 
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appServices.AppServices.Services.CurriculoService;
+import com.appServices.AppServices.Services.CursosService;
 import com.appServices.AppServices.domain.Curriculo;
+import com.appServices.AppServices.domain.Cursos;
+import com.appServices.AppServices.dto.CursosDTO;	
 
 @RestController
 @RequestMapping("prestador/curriculo")
@@ -17,8 +29,12 @@ public class CurriculoResource {
 	@Autowired
 	private CurriculoService service;
 	
+	@Autowired
+	private CursosService serviceCursos;
+	
+	
 	@RequestMapping(value="/{id}",method = RequestMethod.GET)
-	public ResponseEntity<Curriculo> find(@PathVariable Integer id){
+	public ResponseEntity<Curriculo> find(@RequestParam(value="id",defaultValue="1") Integer id){
 		Curriculo objOp = service.find(id);
 		
 		return ResponseEntity.ok().body(objOp);
@@ -26,5 +42,8 @@ public class CurriculoResource {
 	}
 	
 	
+	//---- REST DE CURSOS APARTIR DESSE PONTO----
 	
+	//GET DE CURSO POR ID
+
 }
--- src/main/java/com/appServices/AppServices/resources/CursosResource.java
+++ src/main/java/com/appServices/AppServices/resources/CursosResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appServices.AppServices.Services.CursosService;
@@ -18,22 +19,25 @@ import com.appServices.AppServices.domain.Cursos;
 import com.appServices.AppServices.dto.CursosDTO;
 
 @RestController
-@RequestMapping(value="prestador/curriculo/cursos")
+@RequestMapping(value="prestador/curriculo")
 public class CursosResource {
-
 	@Autowired
 	private CursosService service;
 	
-	@RequestMapping(value="/{id}",method = RequestMethod.GET)
-	public ResponseEntity<Cursos> find(@PathVariable Integer id){
-		Cursos objOp = service.find(id);
-		
-		return ResponseEntity.ok().body(objOp);
+	@RequestMapping(value="/{idCurriculo}/curso",method = RequestMethod.GET)
+	public ResponseEntity<Cursos> findCursos(
+			@PathVariable Integer idCurriculo,
+			@RequestParam(value="id",defaultValue="0") Integer id){
+
+		Cursos objList = service.find(id);
+		return ResponseEntity.ok().body(objList);
 	}
 	
-	
-	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
-	public ResponseEntity<Cursos> update(@Valid @RequestBody CursosDTO objDTO, @PathVariable Integer id){
+	@RequestMapping(value="/{idCurriculo}/curso",method = RequestMethod.PUT)
+	public ResponseEntity<Cursos> update(
+			@Valid @RequestBody CursosDTO objDTO, 
+			@PathVariable Integer idCurriculo,
+			@RequestParam(value="id",defaultValue="0") Integer id){
 		
 		Cursos obj = service.fromDTO(objDTO);
 		obj.setId(id);
@@ -43,17 +47,22 @@ public class CursosResource {
 		return ResponseEntity.noContent().build();			
 	}
 	
-	@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
+	@RequestMapping(value="/{idCurriculo}/curso",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(
+			@PathVariable Integer idCurriculo,
+			@RequestParam(value="id",defaultValue="0") Integer id){
 		service.delete(id);
 		
 		return ResponseEntity.noContent().build();
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<CursosDTO>> findAll(){
+	//GET DE TODOS OS CURSOS
+	@RequestMapping(value="/{idCurriculo}/cursos",method = RequestMethod.GET)
+	public ResponseEntity<List<CursosDTO>> findCursosAll(){
 		List<Cursos> objList = service.findAll();
 		List<CursosDTO> listDto = objList.stream().map(obj -> new CursosDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
 }
